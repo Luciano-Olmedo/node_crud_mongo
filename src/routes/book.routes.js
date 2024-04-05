8cd8d5225c1f3bfb0d669e5478fdd53b96f2a84f
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-04-05 20:23:39 -0300

    add routes put and delete

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -10,7 +10,7 @@ const getBook = async (req, res, next) => {
         return res.status(404).json({ message: "El ID del libro no es válido" })
     }
     try {
-        book = await Book.findbyid(id)
+        book = await Book.findById(id)
         if (!book) {
             return res.status(404).json({ message: 'El libro no fue encontrado' })
         }
@@ -27,11 +27,13 @@ const getBook = async (req, res, next) => {
 
 router.get('/', async (req, res) => {
     try {
-        const books = await Book.find()
+        const books = await Book.find();
+
         if (books.lenght === 0) {
             return res.status(204).json([])
         }
         res.json(books)
+
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
@@ -64,6 +66,56 @@ router.post('/', async (req, res) => {
     }
 
 })
+//obtener un libro por id
+
+router.get('/:id', getBook, async (req, res) => {
+    res.json(res.book);
+}
+)
+
+
+//modificar todos los campos
+router.put('/:id'), getBook, async (req, res) => {
+
+    try {
+        const book = res.book
+        book.title = req.body.title || book.title
+        book.author = req.body.author || book.author
+        book.genre = req.body.genre || book.genre
+        book.publication_date = req.body.publication_date || book.publication_date
+
+        const updateBook = await book.save()
+        res.json(updateBook)
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        })
+    }
+}
+
+//Eliminar un libro
+router.delete('/:id', getBook, async (req, res) => {
+    try {
+        const book = res.book
+        await book.deleteOne({
+            _id: book._id
+        })
+        res.json({
+            message: `El libro ${book.title} fue eliminado correctamente`
+        })
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        })
+    }
+
+
+}
+
+)
 
 module.exports = router;
 
